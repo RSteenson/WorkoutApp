6da02d0a5982411830ce40ba6f4758f1d3859946
Rachel Steenson <r.steenson@hotmail.co.uk>
2021-01-05 14:50:44 +0000

    Add server controls for main workout

--- server.R
+++ server.R
@@ -32,4 +32,38 @@ shinyServer(function(input, output) {
     output$warmup_list <- renderUI(HTML(WU_rv$exercises))
 
 
+    #----- Set workout ---------------------------------------------------------
+
+    # Create reactive values object
+    WO_rv <- reactiveValues(length = "", exercises = "")
+
+    # Observe changes to the total length of the main workout
+    WO_length = reactive({
+        set_length = input$workout_number * (input$workout_ex_length + input$workout_rest_length)
+        total_length = (set_length * input$workout_sets) + (input$workout_interim * (input$workout_sets - 1))
+        total_length = total_length/60
+        total_length
+    })
+    observe({
+        WO_rv$length <- paste0("<h2>", WO_length(), "mins</h2>",
+                               "<h4>", input$workout_sets, " sets of ", input$workout_number, " exercises @ ",
+                               input$workout_ex_length, "s work : ", input$workout_rest_length, "s rest</h4>")
+    })
+
+    # Observe changes to the number of workout exercises
+    observe({
+        workout <- set_exercises(n_ex = input$workout_number)
+        WO_rv$exercises <- paste0("<center><p style='font-size: 17px'><br>", paste0(workout$Exercise, collapse = "<br>"), "<br></p></center>")
+    })
+
+    # If user clicks button, reselect workout exercises
+    observeEvent(input$workout_go, {
+        workout <- set_exercises(n_ex = input$workout_number)
+        WO_rv$exercises <- paste0("<center><p style='font-size: 17px'><br>", paste0(workout$Exercise, collapse = "<br>"), "<br></p></center>")
+    })
+
+    # Create outputs
+    output$workout_header <- renderUI(HTML(WO_rv$length))
+    output$workout_list <- renderUI(HTML(WO_rv$exercises))
+
 })
