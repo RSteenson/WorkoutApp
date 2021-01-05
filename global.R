4335a37cdd1d1855f6ae7113e3aa37d74fa7caba
Rachel Steenson <r.steenson@hotmail.co.uk>
2021-01-05 13:53:28 +0000

    Setup objects

---
+++ global.R
@@ -0,0 +1,22 @@
+# Load libraries
+library(shiny)
+library(shinydashboard)
+library(shinydashboardPlus)
+library(shinyWidgets)
+library(dplyr)
+library(stringr)
+library(rmarkdown)
+library(htmltools)
+library(knitr)
+library(kableExtra)
+
+# Load functions
+source("R/set_exercises.R")
+
+# Load data
+exercises <- read.csv("data/exercises.csv", stringsAsFactors = FALSE)
+
+# Identify current numbers of each exercise type
+n_warmup = length(which(exercises$Type=="Warm-up"))
+n_exercises = nrow(exercises[-which(exercises$Type %in% c("Warm-up", "Cool-down")),])
+n_cooldown = length(which(exercises$Type=="Cool-down"))
