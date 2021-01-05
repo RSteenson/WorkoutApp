0041c8c736cfa47bb460d78115dea202ab2fbe1e
Rachel Steenson <r.steenson@hotmail.co.uk>
2021-01-05 14:50:34 +0000

    Add main workout widgets

--- ui.R
+++ ui.R
@@ -50,10 +50,58 @@ shinyUI(
                     )
                 )
 
-            )
+            ),
 
             #----- Setup workout -----------------------------------------------
+            box(width=6,
+                fluidRow(
+                    column(
+                        width=12, align="center",
+                        boxPad(
+                            color = "blue",
+                            descriptionBlock(
+                                header = "Workout",
+                                rightBorder = FALSE)),
+                        br(),
+                        fluidRow(
+                            column(6,
+                                   knobInput(inputId = "workout_ex_length", label = "Length of exercise period (s):",
+                                             value = 40, min = 1, max = 120,
+                                             displayPrevious = TRUE, #height = "100px",
+                                             lineCap = "round", fgColor = "#428BCA", inputColor = "#428BCA")),
+                            column(6,
+                                   knobInput(inputId = "workout_rest_length", label = "Length of rest period (s):",
+                                             value = 20, min = 1, max = 120,
+                                             displayPrevious = TRUE, #height = "100px",
+                                             lineCap = "round", fgColor = "#428BCA", inputColor = "#428BCA"))
+                        ),
+                        fluidRow(
+                            column(4,
+                                   knobInput(inputId = "workout_number", label = "Number of exercises:",
+                                             value = 5, min = 1, max = n_exercises,
+                                             displayPrevious = TRUE, #height = "100px",
+                                             lineCap = "round", fgColor = "#428BCA", inputColor = "#428BCA")),
+                            column(4,
+                                   knobInput(inputId = "workout_sets", label = "Number of sets:",
+                                             value = 2, min = 1, max = 10,
+                                             displayPrevious = TRUE, #height = "100px",
+                                             lineCap = "round", fgColor = "#428BCA", inputColor = "#428BCA")),
+                            column(4,
+                                   knobInput(inputId = "workout_interim", label = "Length of time between sets (s):",
+                                             value = 60, min = 1, max = 120,
+                                             displayPrevious = TRUE, #height = "100px",
+                                             lineCap = "round", fgColor = "#428BCA", inputColor = "#428BCA"))
+                        ),
+                        boxPad(
+                            color = "blue",
+                            htmlOutput("workout_header"),
+                            htmlOutput("workout_list"),
+                            actionButton(inputId = "workout_go", label = "Choose alternative exercises")
+                        )
+                    )
+                )
 
+            )
             #----- Setup cool-down ---------------------------------------------
 
         )
